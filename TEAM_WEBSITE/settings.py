167507b9c39767520192d8d14f34150f51e54f39
LeftyEzra <solomonezrababalefty25@gmail.com>
2026-06-14 22:35:47 +0100

    fix: turn off strict whitenoise manifest checking

--- TEAM_WEBSITE/settings.py
+++ TEAM_WEBSITE/settings.py
@@ -146,6 +146,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 """
 
+
+
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
