bf7396912cded3a21d59ccbd74b8fa0cd52282ab
LeftyEzra <solomonezrababalefty25@gmail.com>
2026-06-16 00:29:24 +0100

    chore: user password override

--- TEAM_WEBSITE/urls.py
+++ TEAM_WEBSITE/urls.py
@@ -22,3 +22,18 @@ urlpatterns = [
 admin.site.site_header = "Team Administrative Section"
 admin.site.site_title = "StatsHub"
 admin.site.index_title = "Welcome To The StatsHub Admin Area..."
+
+
+# At the very bottom of your urls.py file
+
+from django.contrib.auth import get_user_model
+
+try:
+    User = get_user_model()
+    # Find your account by your username
+    user = User.objects.get(username='admin')  # <-- Change 'admin' to your username if it's different
+    user.set_password('MyNewSecurePassword123!')  # <-- Put the new password you want here
+    user.save()
+    print("PASSWORD RESET SUCCESSFULLY!")
+except Exception as e:
+    print("Password reset error:", e)
