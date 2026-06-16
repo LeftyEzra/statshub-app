7b302d5197c0c6ef8ea6c2e5bf8301bef643cc2c
LeftyEzra <solomonezrababalefty25@gmail.com>
2026-06-16 01:44:37 +0100

    ui: install and configure jazzmin admin theme

--- TEAM_WEBSITE/settings.py
+++ TEAM_WEBSITE/settings.py
@@ -26,6 +26,7 @@ ALLOWED_HOSTS = [host.strip() for host in config("ALLOWED_HOSTS").split(",")]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
